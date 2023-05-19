507b76cbff87611824d8d09c410a45dab036bd72
The_WildThomas <thb5309@gmail.com>
2023-05-19 10:14:00 +0200

    V3.0
    added a light/dark theme
    modified all pages to implement this
    optimized nav and pages
    moved the nav in a special header block
    modified main.py to prevent bugs with the css files
    moved all head items in the nav file (include)
    modified all css to implement vars for the theme modifier

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from custom_md_to_html import *
 
 port = 20011
@@ -7,15 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Accueil", stylesheet='index.css')
+    return render_template('index.html', title="Accueil", stylesheet='index')
+
+@app.route('/theme/<theme>')
+def set_theme(theme):
+    response = redirect('/')
+    response.set_cookie('page_theme', theme)
+    return response
 
 @app.route('/faq')
 def faq():
-    return render_template('faq.html', title="FAQ", stylesheet='faq.css')
+    return render_template('faq.html', title="FAQ", stylesheet='faq')
 
 @app.route('/ressources')
 def ressources():
-    return render_template('ressources.html', title="Ressources", stylesheet='ressources.css')
+    return render_template('ressources.html', title="Ressources", stylesheet='ressources')
 
 @app.route('/presentation', methods=['GET'])
 def presentation():
@@ -23,9 +29,9 @@ def presentation():
     
     if request.args.get("page") in [file[1] for file in files_n_titles]:
         file = f'/pages/{[file[0] for file in files_n_titles if file[1] == request.args.get("page")][0]}'
-        return render_template('presentation.html', is_arg=True, title=request.args.get("page"), stylesheet='presentation.css', file_name=file)
+        return render_template('presentation.html', is_arg=True, title=request.args.get("page"), stylesheet='presentation', file_name=file)
     
-    return render_template('presentation.html', is_arg=False, title='Présentation', stylesheet='presentation.css', files_n_titles=files_n_titles)
+    return render_template('presentation.html', is_arg=False, title='Présentation', stylesheet='presentation', files_n_titles=files_n_titles)
 
 
 
